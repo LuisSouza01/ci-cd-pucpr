22c96d5ec30906461f5c45ddef90587b3761acbb
LuisSouza01 <luis.f.souza@outlook.com>
2026-08-23 13:39:10 -0300

    feat: initial typescript project

---
+++ src/index.ts
@@ -0,0 +1,8 @@
+export function sum(a: number, b: number): number {
+  return a + b;
+}
+
+export function isPalindrome(text: string): boolean {
+  const normalized = text.toLowerCase().replace(/[^a-z0-9]/g, "");
+  return normalized === normalized.split("").reverse().join("");
+}

477eb1bdd4ab91dcc3b23ac7eac076116a8026c3
LuisSouza01 <luis.f.souza@outlook.com>
2026-09-07 09:21:18 -0300

    test discord

--- src/index.ts
+++ src/index.ts
@@ -4,5 +4,6 @@ export function sum(a: number, b: number): number {
 
 export function isPalindrome(text: string): boolean {
   const normalized = text.toLowerCase().replace(/[^a-z0-9]/g, "");
+  
   return normalized === normalized.split("").reverse().join("");
 }
